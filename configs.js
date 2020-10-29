99079e500b2400ef119be1a6d2e2a63078128cde
Promise-Cheng <737362053@qq.com>
2020-10-29 15:37:42 +0800

    跨域访问

--- configs.js
+++ configs.js
@@ -1,8 +1,8 @@
 const DB={
-    host:"localhost",
+    host:"47.99.180.62",
     port:3306,
     user:"root",
-    password:"caoqiubin",
+    password:"1979118cnmbyzwz",
     database:"compwebproj"
 }
 module.exports=DB
